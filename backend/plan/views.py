86d525a3468f13ab31895a50d7454ee0eae4fcd7
Lemarais <goehd4551@gmail.com>
2020-11-05 03:38:16 +0900

    create dummy plan api for test

--- backend/plan/views.py
+++ backend/plan/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from common.util.auth_util import login_required
+from common.util.http_util import HttpStatusCode
+
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.http import require_http_methods
+
+
+# dummy plan function for test
+@ensure_csrf_cookie
+@require_http_methods(['POST'])
+@login_required
+def plan(request):
+    return HttpResponse(status=HttpStatusCode.NoContent)
