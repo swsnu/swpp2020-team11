aa45be954513c13da5e3eb1c17d05564411d4b9d
Lemarais <goehd4551@gmail.com>
2020-11-11 03:29:45 +0900

    add dummy api for test

--- backend/plan/urls.py
+++ backend/plan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import plan
+from .views import plan, reservation
 
 urlpatterns = [
     path('', plan, name='plan'),
+    path('reservation/', reservation, name='reservation'),
 ]
